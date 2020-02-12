29a91705e9bc4b141a7f445bf6e1ebb3f89a3e82
ryanwfin <ryan.finch4416@gmail.com>
2020-02-12 14:35:37 -0700

    Small touches and code fixes

--- js/main.js
+++ js/main.js
@@ -34,8 +34,7 @@ const moves ={
     blackJump: {
         x:[-1, -1],
         y:[-1, 1]
-    },
-    
+    }, 
 }
 
 /* -----app state(variables)-----*/
@@ -66,25 +65,26 @@ let message = document.querySelector('h1');
 document.querySelector('button').addEventListener('click', play);
 
 /* -----functions-----*/
+//checker and square selection, and sends needed information to the jumps and moves functions
 function checkerSelection(evt){
     if(scores.winner !== null) return;
     evt.stopImmediatePropagation();
     const target = evt.target;
-
+    
     if((target.attributes.player.value !== playerTurn) && (pieceSelected === true)){
         console.log('kingMove')
         if(selectedPieceArray[0].classList.contains('red-king') || selectedPieceArray[0].classList.contains('black-king')){
             kingJump(target, selectedPieceArray);
-            pieceSelected = [];
+            pieceSelected = false;
             return;
         }
         if(playerTurn === 'red'){
             redJump(target,selectedPieceArray);
-            pieceSelected = [];
+            pieceSelected = false;
             return;
         }else if(playerTurn === 'black'){
             blackJump(target,selectedPieceArray);
-            pieceSelected = []
+            pieceSelected = false;
             return;
         }
     }else if(pieceSelected === true){
@@ -95,12 +95,9 @@ function checkerSelection(evt){
 
     selectedPieceArray.push(target)
     pieceSelected = true;
-    console.log(selectedPieceArray)
-    
-
 }
+
 function selectSquare(evt){
-    console.log(evt.target)
     const targetSquare = evt.target;
     const targetPiece = selectedPieceArray[0];
     if(pieceSelected === false) return
@@ -123,6 +120,8 @@ function selectSquare(evt){
         pieceSelected = false;
     }
 }
+
+//These are all the jump funtcions, red and black get the information needed and then call jump function for jump. King Jump calls both red and black jump
 function redJump(checkerToJump, checkerJumping){
     const a = checkerToJump.getAttribute('position');
     const x = a[0];
@@ -133,6 +132,21 @@ function redJump(checkerToJump, checkerJumping){
     const coordinateY1= moves.redJump.y[1] + parseInt(y);
 
     jump(coordinateX,coordinateX1,coordinateY,coordinateY1,checkerToJump,checkerJumping);
+}
+function blackJump(checkerToJump, checkerJumping){
+    const a = checkerToJump.getAttribute('position');
+    const x = a[0];
+    const y = a[2];
+    const coordinateX = moves.blackJump.x[0] + parseInt(x);
+    const coordinateY = moves.blackJump.x[1] + parseInt(y);
+    const coordinateX1 = moves.blackJump.y[0] + parseInt(x);
+    const coordinateY1= moves.blackJump.y[1] + parseInt(y);
+
+    jump(coordinateX,coordinateX1,coordinateY,coordinateY1,checkerToJump,checkerJumping);
+}
+function kingJump(checkerToJump, checkerJumping){
+    blackJump(checkerToJump,checkerJumping);
+    redJump(checkerToJump, checkerJumping);
 }
 function jump(coordinateX,coordinateX1,coordinateY,coordinateY1,checkerToJump, checkerJumping){
 
@@ -191,21 +205,8 @@ function jump(coordinateX,coordinateX1,coordinateY,coordinateY1,checkerToJump, c
         }
     }
 }
-function blackJump(checkerToJump, checkerJumping){
-    const a = checkerToJump.getAttribute('position');
-    const x = a[0];
-    const y = a[2];
-    const coordinateX = moves.blackJump.x[0] + parseInt(x);
-    const coordinateY = moves.blackJump.x[1] + parseInt(y);
-    const coordinateX1 = moves.blackJump.y[0] + parseInt(x);
-    const coordinateY1= moves.blackJump.y[1] + parseInt(y);
 
-    jump(coordinateX,coordinateX1,coordinateY,coordinateY1,checkerToJump,checkerJumping);
-}
-function kingJump(checkerToJump, checkerJumping){
-    blackJump(checkerToJump,checkerJumping);
-    redJump(checkerToJump, checkerJumping);
-}
+//These are the move functions, red and black feed needed info to move. King move calls both red and black move functions
 function kingMove(square, checker){
     redMove(square, checker);
     blackMove(square ,checker);
@@ -219,6 +220,14 @@ function redMove(square, checker){
    move(square, checker, x, y);
    
 }
+function blackMove(square, checker){
+    let a = checker.getAttribute('position')
+    let b = square.getAttribute('position')
+    let x = a[0] - b[0];
+    let y = a[2] - b[2];
+
+    move(square, checker, x, y);
+} 
 function move(square, checker, x, y){
     if(checker.getAttribute('player') === 'red' || checker.classList.contains('red-king') || checker.classList.contains('black-king')){
         if((moves.redMove.x[0] === x && moves.redMove.x[1] === y) || moves.redMove.y[0] === x && moves.redMove.y[1] === y){
@@ -242,23 +251,20 @@ function move(square, checker, x, y){
             return selectedPieceArray = [];
         }
     }
-}
-function blackMove(square, checker){
-    let a = checker.getAttribute('position')
-    let b = square.getAttribute('position')
-    let x = a[0] - b[0];
-    let y = a[2] - b[2];
+}  
 
-    move(square, checker, x, y);
-}   
+//render calls most states of teh board for porper displaying and resetting of some variables
 function render(){
     changeTurn();
     renderScores();
     displayTurn();
     getWinner();
+    
     selectedPieceArray = [];
     pieceSelected = false;
 }
+
+//messages and info for displaying turn
 function displayTurn(){
     if(playerTurn === scores.player1){
         message.innerHTML = `Player 1, it's your turn!`
@@ -268,6 +274,7 @@ function displayTurn(){
     }
 }
 
+//messages for displaying winner and setting winner to not null
 function getWinner(){
     if(scores.redPieceCount === 0){
         scores.winner = scores.player2;
@@ -278,6 +285,7 @@ function getWinner(){
     }
 }
 
+//changes the player turn
 function changeTurn(){
     
         if(playerTurn === scores.player1){
@@ -288,12 +296,15 @@ function changeTurn(){
     
 }
 
+//button click to create pieces and init the board state
 function play(){
     removeCheckers();
     resetScores();
     init(); 
   
 }
+
+//resets scores to initial value
 function resetScores(){
     scores.redPieceCount = 12;
     scores.blackPieceCount = 12
@@ -301,17 +312,17 @@ function resetScores(){
     scores.redPiecesTaken = 0;
     scores.winner = null;
 }
+
+//As it says, initializes the board and HTML
 function init(){
-    console.log('Lets get it on')
     renderScores();
     setBoard();
     createCheckers();
     message.innerHTML = `Player 1! It's your turn!`
     playerTurn = redCheckers.player;
-    console.log(playerTurn)
-    console.log(selectedPieceArray)
-    console.log(pieceSelected)
 }
+
+//maps board with coordinates for location, adds event listener to each board and gives occupied/false attribute
 function setBoard(){
     
     for(let i = 0; i <boardSquares.length; i++){
@@ -322,6 +333,7 @@ function setBoard(){
 
 }
 
+//maps scores to HTML
 function renderScores(){
     scoreElements.rpCount.innerText = scores.redPieceCount;
     scoreElements.bpTaken.innerText = scores.blackPiecesTaken;
@@ -329,22 +341,24 @@ function renderScores(){
     scoreElements.rpTaken.innerText = scores.redPiecesTaken;
 }
 
+//remoevs all checkers from teh board for re-initializing game for replay
 function removeCheckers(){
     let element = document.querySelectorAll('.checker')
         element.forEach(el => {
             el.remove(el);        
     });
 }
+
+//creates all checekers for board and gives need attributes and event listeners
 function createCheckers(){
     
     for(let i = 0; i < 12; i++){
         const checker = document.createElement('div'); 
         checker.setAttribute('class', redCheckers.class)
-        checker.setAttribute('color', redCheckers.color)
+
         checker.setAttribute('player', redCheckers.player)
         checker.setAttribute('position', checkerBoard[i])
         boardSquares[i].appendChild(checker);
-        checker.setAttribute('king', redCheckers.king)
         checker.addEventListener('click', checkerSelection)
         boardSquares[i].setAttribute('occupied', true);
     }
@@ -352,25 +366,24 @@ function createCheckers(){
     for(let i = 20; i < 32; i++){
         const checker = document.createElement('div'); 
         checker.setAttribute('class',  blackCheckers.class);
-        checker.setAttribute('color', blackCheckers.color);
         checker.setAttribute('player', blackCheckers.player);
         boardSquares[i].appendChild(checker);
-        checker.setAttribute('king', blackCheckers.king)
         checker.setAttribute('position',checkerBoard[i] )
         checker.addEventListener('click', checkerSelection)
         boardSquares[i].setAttribute('occupied', true);
     }
 }
+
+//logic for determining if checker becomes a king
 function isKing(checker){
     const kingCheck = checker
-    console.log(kingCheck)
     if(kingCheck.getAttribute('player') === 'red'){
             if(parseInt(kingCheck.getAttribute('position')[0]) === 8){
                 kingCheck.classList.add('red-king');
             }
         }
     if(kingCheck.getAttribute('player') === 'black'){
-            if(parseInt(kingCheck.getAttribute('position')[0]) === 1){console.log(kingCheck)
+            if(parseInt(kingCheck.getAttribute('position')[0]) === 1){
                 kingCheck.classList.add('black-king'); 
             }
         }
